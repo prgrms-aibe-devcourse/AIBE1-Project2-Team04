56a00f616a9a8d066a7a70b9318b2d9dea7af23f
AndBlack99 <jwoo1687@icloud.com>
2025-05-05 17:13:36 +0900

    Reservation.java 추가

---
+++ src/main/java/com/reboot/auth/entity/Reservation.java
@@ -0,0 +1,36 @@
+package com.reboot.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="reservations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "instructor_id")
+    private Long instructorId;
+
+    @Column(name = "lecture_id")
+    private Long lectureId;
+
+    private LocalDateTime date;
+
+    private String status;
+}
