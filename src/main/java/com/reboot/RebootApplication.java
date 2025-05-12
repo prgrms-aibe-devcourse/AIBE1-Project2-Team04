a98c497e45c405e180286baf1d93d3db90e62ec7
LimPark996 <uji835311@gmail.com>
2025-05-13 00:49:35 +0900

    fix: 한 페이지 내 모든 설문폼 들어가도록 변경 완료, 스피너 추가

--- src/main/java/com/reboot/RebootApplication.java
+++ src/main/java/com/reboot/RebootApplication.java
@@ -45,6 +45,7 @@ package com.reboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class RebootApplication {
